8ce8216544efaa80d9f9841f9de778d946ffdb18
= <mouroum.m@gmail.com>
2020-04-29 18:45:01 +0200

    verification shell

--- Android/UnOHealthApp/app/src/main/java/de/mouroum/uno_health_app/RegisterActivity.kt
+++ Android/UnOHealthApp/app/src/main/java/de/mouroum/uno_health_app/RegisterActivity.kt
@@ -1,12 +1,45 @@
 package de.mouroum.uno_health_app
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.net.URL
 
 class RegisterActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_layout)
+        setContentView(R.layout.registration_activity)
+    }
+
+
+    fun verify(view:View){
+
+        val client = OkHttpClient()
+        val url = URL(UONApp.HOST + "/survey/verify")
+
+        val json = """{
+            
+            }""".trimMargin()
+
+        val body = json.toRequestBody(mediaTypeJson)
+
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
+            .addHeader("Authorization", UONApp.TOKEN)
+            .addHeader("Content-Type","application/json")
+            .build()
+
+        val response = client.newCall(request).execute()
+
+        val responseBody = response.body!!.string()
+        print(response.code)
+
+        //Response
+        println("Response Body: $responseBody")
     }
 }
