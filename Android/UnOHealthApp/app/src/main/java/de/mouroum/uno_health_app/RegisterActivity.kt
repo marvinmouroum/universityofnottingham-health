3694dc828fe51f3df87d452336db4bc74d0f02e7
= <mouroum.m@gmail.com>
2020-04-29 18:46:23 +0200

    added the onclick and the media type to registrationa ctivity

--- Android/UnOHealthApp/app/src/main/java/de/mouroum/uno_health_app/RegisterActivity.kt
+++ Android/UnOHealthApp/app/src/main/java/de/mouroum/uno_health_app/RegisterActivity.kt
@@ -3,6 +3,8 @@ package de.mouroum.uno_health_app
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -15,6 +17,7 @@ class RegisterActivity:AppCompatActivity() {
         setContentView(R.layout.registration_activity)
     }
 
+    private val mediaTypeJson: MediaType? = "application/json; charset=utf-8".toMediaTypeOrNull()
 
     fun verify(view:View){
 
